5f2286f493950f9f2388a22f18786d08989f1a2b
Ryan Guimarães <guimaraesryan26@gmail.com>
2025-06-23 23:59:30 -0300

    feat: add controller to manage service and expose operations
    Also a GlobalExceptionHandler was implemented to ensure the app doesn't fail ugly messages to the user. A great play would be implement logging the errors somewhere else.

---
+++ src/main/java/com/example/todolist/interfaces/rest/TodoController.java
@@ -0,0 +1,66 @@
+package com.example.todolist.interfaces.rest;
+
+import com.example.todolist.application.TodoService;
+import com.example.todolist.application.dto.CreateTodoDto;
+import com.example.todolist.application.dto.TodoResponseDto;
+import com.example.todolist.application.dto.UpdateTodoDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/todos")
+@RequiredArgsConstructor
+public class TodoController {
+    private final TodoService todoService;
+
+    @PostMapping
+    public ResponseEntity<TodoResponseDto> createTodo(@Valid @RequestBody CreateTodoDto request) {
+        TodoResponseDto todo = todoService.createTodo(request);
+        
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(todo.id())
+                .toUri();
+                
+        return ResponseEntity.created(location).body(todo);
+    }
+
+    @GetMapping
+    public List<TodoResponseDto> getAllTodos() {
+        return todoService.getAllTodos();
+    }
+
+
+    @GetMapping("/{id}")
+    public TodoResponseDto getTodoById(@PathVariable UUID id) {
+        return todoService.getTodoById(id);
+    }
+
+    @PutMapping("/{id}")
+    public TodoResponseDto updateTodo(
+            @PathVariable UUID id,
+            @Valid @RequestBody UpdateTodoDto request
+    ) {
+        return todoService.updateTodo(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTodo(@PathVariable UUID id) {
+        todoService.deleteTodo(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/{id}/toggle-status")
+    public TodoResponseDto toggleTodoStatus(@PathVariable UUID id) {
+        return todoService.toggleTodoStatus(id);
+    }
+
+}
