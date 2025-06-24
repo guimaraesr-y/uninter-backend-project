25b32031eb5a63a3322366cdef95b4da630c553b
Ryan Guimarães <guimaraesryan26@gmail.com>
2025-06-23 22:23:05 -0300

    first commit

---
+++ src/main/java/com/example/todolist/TodoListApplication.java
@@ -0,0 +1,13 @@
+package com.example.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.todolist.infrastructure.persistence")
+public class TodoListApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TodoListApplication.class, args);
+    }
+}
