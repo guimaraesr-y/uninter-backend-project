0cfb6d745763c60c18d6c891dce789e73ac9b7d2
Ryan Guimarães <guimaraesryan26@gmail.com>
2025-06-24 23:38:30 -0300

    fix: use jakarta validation
    Removed generic error from GlobalExceptionHandler cause it was causing troubles other expected exceptions.

--- src/main/java/com/uninter/todolist/todolist/common/interfaces/exception/GlobalExceptionHandler.java
+++ src/main/java/com/uninter/todolist/todolist/common/interfaces/exception/GlobalExceptionHandler.java
@@ -3,8 +3,12 @@ package com.uninter.todolist.todolist.common.interfaces.exception;
 import com.uninter.todolist.todolist.common.domain.exception.BaseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -13,22 +17,12 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<Object> handleIllegalArgumentException(
-            IllegalArgumentException ex, WebRequest request) {
-
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("message", ex.getMessage());
-
-        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
-    }
 
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<?> handleApiBaseExceptions(BaseException ex) {
@@ -40,16 +34,22 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(body, ex.getStatus());
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleAllOtherExceptions(
-            Exception ex, WebRequest request) {
-
-        logger.error("Internal server error", ex);
-
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("message", "Internal Server Error");
-
-        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+        Map<String, String> errors = ex.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.toMap(
+                        FieldError::getField,
+                        FieldError::getDefaultMessage,
+                        (msg1, msg2) -> msg1
+                ));
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .headers(headers)
+                .body(errors);
     }
+
 }
--- src/main/java/com/uninter/todolist/todolist/todo/application/dto/CreateTodoDto.java
+++ src/main/java/com/uninter/todolist/todolist/todo/application/dto/CreateTodoDto.java
@@ -1,17 +1,16 @@
 package com.uninter.todolist.todolist.todo.application.dto;
 
+import com.uninter.todolist.todolist.common.domain.exception.ValidationException;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.SneakyThrows;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.time.LocalDateTime;
 
-public record CreateTodoDto(String nome, String responsavel, LocalDateTime dataEntrega) {
-    public CreateTodoDto {
-        if (nome == null || nome.isBlank()) {
-            throw new IllegalArgumentException("Nome não pode ser vazio");
-        }
-        if (responsavel == null || responsavel.isBlank()) {
-            throw new IllegalArgumentException("Responsável não pode ser vazio");
-        }
-        if (dataEntrega == null) {
-            throw new IllegalArgumentException("Data de entrega não pode ser nula");
-        }
-    }
-}
+public record CreateTodoDto(@NotBlank(message = "Nome não pode ser vazio")
+                            String nome,
+                            @NotBlank(message = "Responsável não pode ser vazio")
+                            String responsavel,
+                            @DateTimeFormat @NotNull(message = "Data de entrega é obrigatória")
+                            LocalDateTime dataEntrega) { }
